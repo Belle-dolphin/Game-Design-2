9f21e12ecf4d330ffbd6fafa278b6637d197f315
Loekstars <loekstars@hotmail.com>
2023-12-14 11:18:02 +0100

    Update 4

--- Assets/Scripts/Graphs/RoadValue.cs
+++ Assets/Scripts/Graphs/RoadValue.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RoadValue : MonoBehaviour
 {
     public int weight;
+    //create variable for text mesh pro
+    [SerializeField]
+    private TextMeshProUGUI RoadText;
+
+    void Start()
+    {
+        RoadText.text = weight + "";
+    }
 
     public int getWeight()
     {
