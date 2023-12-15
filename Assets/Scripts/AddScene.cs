971070adc211ec4e0af3c8558e93ed418f785a8d
Serki <s.e.durusu@gmail.com>
2023-12-15 19:57:34 +0100

    AddScene class to add scenes additively

---
+++ Assets/Scripts/AddScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddScene : MonoBehaviour
+{
+    // Name of the scene to be loaded additively
+    public string sceneNameToAdd;
+
+    private void OnMouseDown()
+    {
+        // Check if the scene is not already loaded
+        if (!SceneManager.GetSceneByName(sceneNameToAdd).isLoaded)
+        {
+            // Load the scene additively
+            SceneManager.LoadScene(sceneNameToAdd, LoadSceneMode.Additive);
+        }
+        else
+        {
+            // If already loaded, unload the scene
+            SceneManager.UnloadSceneAsync(sceneNameToAdd);
+        }
+    } 
+}
