9105fa44e93228654f1e8d382542dc0ea4d9978c
Omar Raafat <omarassem2003@hotmail.com>
2024-01-16 11:18:11 +0100

    Making bands

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int multiplier = 1; // Set this to your multiplier
+    public Text moneyText; // Assign this in the inspector
+
+    private float elapsedTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int money = (int)(elapsedTime * multiplier);
+        moneyText.text = "Money: " + money.ToString();
+    }
+}
